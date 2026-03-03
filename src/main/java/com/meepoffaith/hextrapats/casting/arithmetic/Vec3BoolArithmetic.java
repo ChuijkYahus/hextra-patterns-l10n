028121b75661c31b553bc121374506609b36e9b9
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-03-03 01:25:46 -0800

    No .prototype() spam

--- src/main/java/com/meepoffaith/hextrapats/casting/arithmetic/Vec3BoolArithmetic.java
+++ src/main/java/com/meepoffaith/hextrapats/casting/arithmetic/Vec3BoolArithmetic.java
@@ -15,8 +15,7 @@ import net.minecraft.util.math.Vec3d;
 import java.util.List;
 
 import static at.petrak.hexcasting.common.lib.hex.HexIotaTypes.VEC3;
-import static com.meepoffaith.hextrapats.init.Patterns.LEN_EQ;
-import static com.meepoffaith.hextrapats.init.Patterns.LEN_NEQ;
+import static com.meepoffaith.hextrapats.init.Arithmetics.*;
 
 public class Vec3BoolArithmetic implements Arithmetic{
     private static final List<HexPattern> OPS = List.of(
@@ -24,8 +23,8 @@ public class Vec3BoolArithmetic implements Arithmetic{
         LESS,
         GREATER_EQ,
         LESS_EQ,
-        LEN_EQ.prototype(),
-        LEN_NEQ.prototype()
+        LEN_EQ,
+        LEN_NEQ
     );
 
     @Override
@@ -56,9 +55,9 @@ public class Vec3BoolArithmetic implements Arithmetic{
                 double lb = b.length();
                 return DoubleIota.tolerates(la, lb) || la <= lb;
             });
-        }else if(pattern.equals(LEN_EQ.prototype())){
+        }else if(pattern.equals(LEN_EQ)){
             return makeComp((a, b) -> DoubleIota.tolerates(a.length(), b.length()));
-        }else if(pattern.equals(LEN_NEQ.prototype())){
+        }else if(pattern.equals(LEN_NEQ)){
             return makeComp((a, b) -> !DoubleIota.tolerates(a.length(), b.length()));
         }else{
             throw new InvalidOperatorException(pattern + " is not a valid operator in Vec3 Bool Arithmetic " + this);
--- src/main/java/com/meepoffaith/hextrapats/init/Arithmetics.java
+++ src/main/java/com/meepoffaith/hextrapats/init/Arithmetics.java
@@ -1,6 +1,7 @@
 package com.meepoffaith.hextrapats.init;
 
 import at.petrak.hexcasting.api.casting.arithmetic.Arithmetic;
+import at.petrak.hexcasting.api.casting.math.HexPattern;
 import at.petrak.hexcasting.common.lib.hex.HexArithmetics;
 import com.meepoffaith.hextrapats.HextraPatterns;
 import com.meepoffaith.hextrapats.casting.arithmetic.Vec3Arithmetic;
@@ -9,6 +10,9 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class Arithmetics{
+    public static HexPattern LEN_EQ = Patterns.LEN_EQ.prototype();
+    public static HexPattern LEN_NEQ = Patterns.LEN_NEQ.prototype();
+
     public static void init(){
         registerArithmetic("vec3bool", new Vec3BoolArithmetic());
         registerArithmetic("vec3math", new Vec3Arithmetic());
