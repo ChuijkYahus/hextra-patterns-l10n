0c242f93a481263a9bafba287618c0b0a93005c2
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-03-20 07:38:44 -0700

    Langify bad direction message

--- src/main/java/com/meepoffaith/hextrapats/casting/arithmetic/operator/vec3/OperatorTurnVec.java
+++ src/main/java/com/meepoffaith/hextrapats/casting/arithmetic/operator/vec3/OperatorTurnVec.java
@@ -7,9 +7,11 @@ import at.petrak.hexcasting.api.casting.iota.DoubleIota;
 import at.petrak.hexcasting.api.casting.iota.Iota;
 import at.petrak.hexcasting.api.casting.iota.Vec3Iota;
 import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota;
+import at.petrak.hexcasting.api.utils.HexUtils;
 import com.meepoffaith.hextrapats.casting.bases.HexIotaStack;
 import com.meepoffaith.hextrapats.casting.bases.OperatorBase;
 import com.meepoffaith.hextrapats.util.MathUtils;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
@@ -33,7 +35,7 @@ public class OperatorTurnVec extends OperatorBase{
             return asActionResult(new Vec3Iota(to.multiply(from.length() / to.length())));
         }else if(DoubleIota.tolerates(angDist, Math.PI)){
             //From and To are facing directly away from each other. In this case, no axis of rotation can be determined (cross product returns the 0 vector).
-            throw new MishapInvalidIota(stack.get(1), 1, Text.of("vector not facing directly away from start direction"));
+            throw new MishapInvalidIota(stack.get(1), 1, Text.translatable("hextrapats.mishap.bad_direction", stack.get(0).display()));
         }
 
         Vec3d fromN = from.normalize();
