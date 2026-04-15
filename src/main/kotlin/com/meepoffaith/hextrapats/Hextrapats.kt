23fb311e84c5d81ae7acf7322ccdd4da582de98d
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-04-15 16:02:59 -0700

    Some minor setup

--- src/main/kotlin/com/meepoffaith/hextrapats/Hextrapats.kt
+++ src/main/kotlin/com/meepoffaith/hextrapats/Hextrapats.kt
@@ -1,7 +1,8 @@
-package com.meepoffaith
+package com.meepoffaith.hextrapats
 
 import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
+import com.meepoffaith.hextrapats.init
 
 object Hextrapats : ModInitializer {
     private val logger = LoggerFactory.getLogger("hextrapats")
@@ -11,5 +12,7 @@ object Hextrapats : ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		logger.info("Hello Fabric world!")
+
+		Patterns.init();
 	}
 }
---
+++ src/main/kotlin/com/meepoffaith/hextrapats/init/Patterns.kt
@@ -0,0 +1,7 @@
+package com.meepoffaith.hextrapats.init
+
+object Patterns {
+    fun init(){
+
+    }
+}
