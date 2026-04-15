443c20b95d915736d2636c10f1aa8110ef944803
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-04-15 15:38:49 -0700

    Import Fabric Koltin Template

---
+++ src/client/kotlin/com/meepoffaith/client/HextrapatsClient.kt
@@ -0,0 +1,9 @@
+package com.meepoffaith.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object HextrapatsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
