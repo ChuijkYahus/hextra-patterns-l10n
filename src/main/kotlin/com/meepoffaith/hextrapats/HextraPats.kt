c36f56239dd8619fafbf942f1bc9c0813550e134
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-04-15 17:42:13 -0700

    Re-implement conditional exec/halt
    IT COMPILES
    IT RUNS
    RAAAAHHHHHHH

---
+++ src/main/kotlin/com/meepoffaith/hextrapats/HextraPats.kt
@@ -0,0 +1,25 @@
+package com.meepoffaith.hextrapats
+
+import com.meepoffaith.hextrapats.init.Patterns
+import net.fabricmc.api.ModInitializer
+import net.minecraft.util.Identifier
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+object HextraPats : ModInitializer {
+	const val MOD_ID: String = "hextrapats"
+    val LOGGER: Logger = LoggerFactory.getLogger("hextrapats")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		LOGGER.info("Is anyone there? Hello? I'm trAPPED IN HERE AND CAN'T GET OUT H E L P !")
+
+		Patterns.init()
+    }
+
+	fun modLoc(name: String): Identifier {
+		return Identifier(MOD_ID, name)
+    }
+}
--- src/main/kotlin/com/meepoffaith/hextrapats/init/Patterns.kt
+++ src/main/kotlin/com/meepoffaith/hextrapats/init/Patterns.kt
@@ -1,7 +1,71 @@
 package com.meepoffaith.hextrapats.init
 
-object Patterns {
-    fun init(){
+import at.petrak.hexcasting.api.casting.ActionRegistryEntry
+import at.petrak.hexcasting.api.casting.castables.Action
+import at.petrak.hexcasting.api.casting.castables.OperationAction
+import at.petrak.hexcasting.api.casting.castables.SpecialHandler
+import at.petrak.hexcasting.api.casting.math.HexDir
+import at.petrak.hexcasting.api.casting.math.HexPattern
+import at.petrak.hexcasting.common.lib.hex.HexActions
+import at.petrak.hexcasting.xplat.IXplatAbstractions
+import com.meepoffaith.hextrapats.HextraPats
+import com.meepoffaith.hextrapats.casting.actions.NoConsOperationAction
+import com.meepoffaith.hextrapats.casting.actions.eval.OpConditionalEval
+import com.meepoffaith.hextrapats.casting.actions.eval.OpConditionalHalt
+import net.minecraft.registry.Registry
 
+object Patterns {
+    /*
+    val ROT_ABOUT_X = register("rot_about_x", "aaqqqqqea", HexDir.SOUTH_WEST)
+    val ROT_ABOUT_Y = register("rot_about_x", "aaqqqqqew", HexDir.SOUTH_WEST)
+    val ROT_ABOUT_Z = register("rot_about_x", "aaqqqqqed", HexDir.SOUTH_WEST)
+    val CONSTRUCT_ABOUT_X = register("cons_about_x", "daqqqqqea", HexDir.NORTH_WEST)
+    val CONSTRUCT_ABOUT_Y = register("cons_about_y", "daqqqqqew", HexDir.NORTH_WEST)
+    val CONSTRUCT_ABOUT_Z = register("cons_about_z", "daqqqqqed", HexDir.NORTH_WEST)
+    val NORMALIZE = register("normalize", "eeeeedww", HexDir.SOUTH_WEST)
+    val LEN_EQ = register("len_eq", "adqqaqw", HexDir.EAST)
+    val LEN_NEQ = register("len_neq", "daeedew", HexDir.EAST)
+    val IN_RANGE = register("in_range", "qqqq", HexDir.SOUTH_WEST)
+    val OUT_RANGE = register("out_range", "eaae", HexDir.SOUTH_EAST)
+    val INVERT = register("invert", "waqawqa", HexDir.SOUTH_WEST)
+    val INCREMENT = register("increment", "waawawaaw", HexDir.NORTH_EAST)
+    val DECREMENT = register("decrement", "wddwdwddw", HexDir.NORTH_WEST)
+    val APPROACH = register("approach", "dedqadeeed", HexDir.SOUTH_WEST)
+    val ANGLE_DIST = register("angle_dist", "awdaqqqqqea", HexDir.NORTH_EAST)
+    val ANGLE_APPROACH = register("angle_approach", "awdaqqqqqwd", HexDir.NORTH_EAST)
+    val SET_INSERT_RET = register("set_insert_ret", "edqdewd", HexDir.SOUTH_WEST )
+    val SET_REMOVE_RET = register("set_remove_ret", "edqdewaqaaed", HexDir.SOUTH_WEST)
+     */
+
+    fun init(){
+        register("true_eval", "deaqqaaqa", HexDir.SOUTH_EAST, OpConditionalEval(true))
+        register("false_eval", "deaqqdded", HexDir.SOUTH_EAST, OpConditionalEval(false))
+        register("true_halt", "aqdeedded", HexDir.SOUTH_WEST, OpConditionalHalt(true))
+        register("false_halt", "aqdeeaaqa", HexDir.SOUTH_WEST, OpConditionalHalt(false))
+    }
+
+    private fun register(name: String, signature: String, startDir: HexDir, action: Action){
+        Registry.register(HexActions.REGISTRY, HextraPats.modLoc(name),
+            ActionRegistryEntry(HexPattern.fromAngles(signature, startDir), action)
+        )
+    }
+
+    private fun register(name: String, signature: String, startDir: HexDir): HexPattern {
+        val pattern = HexPattern.fromAngles(signature, startDir)
+        Registry.register(HexActions.REGISTRY, HextraPats.modLoc(name),
+            ActionRegistryEntry(pattern, OperationAction(pattern))
+        )
+        return pattern
+    }
+
+    private fun <T : SpecialHandler> registerSpecialHandler(name: String, handler: SpecialHandler.Factory<T>){
+        Registry.register(IXplatAbstractions.INSTANCE.specialHandlerRegistry, HextraPats.modLoc(name), handler);
+    }
+
+    private fun registerNoConsumeOp(name: String, signature: String, startDir: HexDir, copied: HexPattern, argc: Int){
+        val pattern = HexPattern.fromAngles(signature, startDir)
+        Registry.register(HexActions.REGISTRY, HextraPats.modLoc(name),
+            ActionRegistryEntry(pattern, NoConsOperationAction(pattern, argc))
+        )
     }
 }
