599beb8cb15ffaa4ee636da9fddf883ee0fdf2cd
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-04-20 16:06:08 -0700

    Copy over scientific exponent

--- src/main/kotlin/com/meepoffaith/hextrapats/init/SpecialHandlers.kt
+++ src/main/kotlin/com/meepoffaith/hextrapats/init/SpecialHandlers.kt
@@ -3,6 +3,7 @@ package com.meepoffaith.hextrapats.init
 import at.petrak.hexcasting.api.casting.castables.SpecialHandler
 import at.petrak.hexcasting.xplat.IXplatAbstractions
 import com.meepoffaith.hextrapats.HextraPats
+import com.meepoffaith.hextrapats.casting.actions.handlers.ScientificExponent
 import com.meepoffaith.hextrapats.casting.actions.handlers.Vector1
 import com.meepoffaith.hextrapats.casting.actions.handlers.VectorX
 import com.meepoffaith.hextrapats.casting.actions.handlers.VectorY
@@ -14,6 +15,7 @@ object SpecialHandlers {
     val VEC_Y = registerSpecialHandler("vec_y", VectorY.Factory())
     val VEC_Z = registerSpecialHandler("vec_z", VectorZ.Factory())
     val VEC_1 = registerSpecialHandler("vec_1", Vector1.Factory())
+    val SCI_EXP = registerSpecialHandler("scientific_exp", ScientificExponent.Factory())
 
     fun init(){}
 
